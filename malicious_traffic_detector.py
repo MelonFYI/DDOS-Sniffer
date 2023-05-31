8b20056f7badc6ada7d4a9b70f20703ee3d2f5bd
Callum Jones <100067014+MelonFYI@users.noreply.github.com>
2023-05-31 16:06:56 +0100

    Update malicious_traffic_detector.py
    attempt to change IP if dynamic via basic concept of ipconfig

--- malicious_traffic_detector.py
+++ malicious_traffic_detector.py
@@ -1,3 +1,4 @@
+import subprocess
 from scapy.all import *
 from win10toast import ToastNotifier
 
@@ -8,8 +9,29 @@ toaster = ToastNotifier()
 def packet_callback(packet):
     # Check if the packet contains malicious traffic based on your criteria
     if packet.haslayer(TCP) and packet[TCP].flags == 2:
+        # Extract relevant information from the packet
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
+
+        # Prepare the notification message
+        message = f"Malicious Traffic Detected!\n\nSource IP: {src_ip}\nDestination IP: {dst_ip}\nSource Port: {src_port}\nDestination Port: {dst_port}"
+
         # Display a toast notification
-        toaster.show_toast("Malicious Traffic Detected", "Your PC received a malicious TCP packet!", duration=10)
+        toaster.show_toast("Security Alert", message, duration=10)
+
+        # Ask the user if they want to change their IP address
+        change_ip = input("Do you want to attempt changing your IP address? (y/n): ")
+        if change_ip.lower() == "y":
+            # Attempt to release and renew the IP address
+            release_ip_cmd = "ipconfig /release"
+            renew_ip_cmd = "ipconfig /renew"
+
+            # Execute the commands using subprocess
+            subprocess.run(release_ip_cmd, shell=True)
+            subprocess.run(renew_ip_cmd, shell=True)
+            print("IP address changed successfully.")
 
 # Sniff packets on the network interface
 sniff(prn=packet_callback, filter="tcp")
@@ -17,3 +39,4 @@ sniff(prn=packet_callback, filter="tcp")
 # Run the program indefinitely
 while True:
     pass
+
